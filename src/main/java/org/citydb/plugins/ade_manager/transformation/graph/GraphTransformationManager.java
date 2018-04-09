229ffa67ca8676e43835cd3896e265f566b8ab61
Zhihang Yao <zyao@virtualcitysystems.de>
2018-04-09 09:48:44 +0200

    convert all database object's names to lower case

--- src/main/java/org/citydb/plugins/ade_manager/transformation/graph/GraphTransformationManager.java
+++ src/main/java/org/citydb/plugins/ade_manager/transformation/graph/GraphTransformationManager.java
@@ -7,18 +7,25 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.util.Enumeration;
+import java.util.List;
 
 import org.citydb.log.Logger;
 import org.citydb.plugins.ade_manager.config.ConfigImpl;
 import org.citygml4j.xml.schema.Schema;
 import org.citygml4j.xml.schema.SchemaHandler;
 
+import agg.attribute.AttrInstance;
+import agg.attribute.impl.ValueMember;
+import agg.attribute.impl.ValueTuple;
 import agg.editor.impl.EdGraGra;
 import agg.util.XMLHelper;
 import agg.xt_basis.BaseFactory;
 import agg.xt_basis.GraGra;
 import agg.xt_basis.GraTra;
 import agg.xt_basis.LayeredGraTraImpl;
+import agg.xt_basis.Node;
+import agg.xt_basis.Type;
 
 public class GraphTransformationManager {
 	private final Logger LOG = Logger.getInstance();
@@ -51,6 +58,11 @@ public class GraphTransformationManager {
 
     	// execute graph transformation
     	graTra.transform();
+    	
+    	// post process for converting every database object's name to lower case
+    	convertDbObjectNameToLowercase();
+    	
+    	// write to file
     	marshallingEdGraphGrammer(config.getTmpGraphDirPath() + File.separator + "Output_Graph_Tmp.ggx");
     	
     	return aggGraphGrammar;
@@ -93,5 +105,22 @@ public class GraphTransformationManager {
 			LOG.info("Graph has been created.");
 		}
 	}	
+	
+	private void convertDbObjectNameToLowercase() {
+		Enumeration<Type> e = edGraphGrammar.getBasisGraGra().getTypes();		
+		while(e.hasMoreElements()){
+			Type nodeType = e.nextElement();
+			if (nodeType.getName().equalsIgnoreCase(GraphNodeArcType.DatabaseObject)) {
+				List<Node> nodes = edGraphGrammar.getBasisGraGra().getGraph().getNodes(nodeType);
+				for (Node node: nodes) {					
+					AttrInstance attrInstance = node.getAttribute();
+					String name = (String)attrInstance.getValueAt("name");
+					ValueTuple valueTuple = (ValueTuple) attrInstance;
+					ValueMember attr = (ValueMember) valueTuple.getValueMemberAt("name");
+					attr.setExprAsObject(name.toLowerCase());
+				}				
+			};
+		}
+	}
 		
 }
