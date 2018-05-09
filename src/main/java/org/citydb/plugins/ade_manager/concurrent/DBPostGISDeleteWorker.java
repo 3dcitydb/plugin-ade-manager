79130e7f61d3980f88fd5dfab0f7b536f916a583
Zhihang Yao <zyao@virtualcitysystems.de>
2018-05-09 09:59:47 +0200

    use executeQuery for PostGIS since executeBatch may lead to Deadlock

--- src/main/java/org/citydb/plugins/ade_manager/concurrent/DBPostGISDeleteWorker.java
+++ src/main/java/org/citydb/plugins/ade_manager/concurrent/DBPostGISDeleteWorker.java
@@ -7,8 +7,7 @@ import org.citydb.event.EventDispatcher;
 
 public class DBPostGISDeleteWorker extends DBDeleteWorker {
 	private PreparedStatement deleteStatement;
-	private int batchSize = 20;
-	
+
 	public DBPostGISDeleteWorker(EventDispatcher eventDispatcher) throws SQLException {
 		super(eventDispatcher);
 		deleteStatement = connection.prepareStatement("select " + dbSchema + ".del_cityobject(array_agg(?))");	
@@ -16,26 +15,15 @@ public class DBPostGISDeleteWorker extends DBDeleteWorker {
 
 	@Override
 	protected void deleteCityObject(long objectId) throws SQLException {
-		deleteCounter++;
 		deleteStatement.setInt(1, (int)objectId);
-		deleteStatement.addBatch();
-		if (deleteCounter == batchSize) {
-			deleteStatement.executeBatch();
-			deleteCounter = 0;
-		}				
+		deleteStatement.executeQuery();
+			
 	}
 
 	@Override
 	protected void closeDBStatement() throws SQLException {
 		if (deleteStatement != null)
-			deleteStatement.close();	
-			
-	}
-
-	@Override
-	protected void postCommit() throws SQLException {
-		if (deleteCounter > 0)
-			deleteStatement.executeBatch();		
+			deleteStatement.close();			
 	}
 
 }
