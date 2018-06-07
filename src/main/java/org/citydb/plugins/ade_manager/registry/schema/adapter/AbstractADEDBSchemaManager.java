053f3d1204a70ef66503e7bb3313e98f4e6bf1c3
Zhihang Yao <zyao@virtualcitysystems.de>
2018-06-07 15:31:05 +0200

    fixed issue of cleaning up false ADE data when deregistering an ADE

--- src/main/java/org/citydb/plugins/ade_manager/registry/schema/adapter/AbstractADEDBSchemaManager.java
+++ src/main/java/org/citydb/plugins/ade_manager/registry/schema/adapter/AbstractADEDBSchemaManager.java
@@ -5,25 +5,20 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.locks.ReentrantLock;
 
-import org.citydb.config.ConfigNamespaceFilter;
-import org.citydb.config.project.exporter.SimpleQuery;
-import org.citydb.database.adapter.AbstractDatabaseAdapter;
+import org.citydb.citygml.exporter.database.content.DBSplittingResult;
 import org.citydb.database.connection.DatabaseConnectionPool;
 import org.citydb.database.schema.mapping.AbstractObjectType;
 import org.citydb.database.schema.mapping.SchemaMapping;
 import org.citydb.log.Logger;
 import org.citydb.plugins.ade_manager.config.ConfigImpl;
-import org.citydb.plugins.ade_manager.delete.DBDeleteController;
-import org.citydb.plugins.ade_manager.delete.DBDeleteException;
+import org.citydb.plugins.ade_manager.delete.DBDeleteWorker;
 import org.citydb.plugins.ade_manager.registry.metadata.ADEMetadataManager;
 import org.citydb.plugins.ade_manager.registry.schema.ADEDBSchemaManager;
 import org.citydb.plugins.ade_manager.registry.schema.SQLScriptRunner;
-import org.citydb.query.Query;
-import org.citydb.query.builder.QueryBuildException;
-import org.citydb.query.builder.config.ConfigQueryBuilder;
-import org.citydb.query.filter.projection.ProjectionFilter;
 import org.citydb.registry.ObjectRegistry;
 
 public abstract class AbstractADEDBSchemaManager implements ADEDBSchemaManager {
@@ -60,48 +55,43 @@ public abstract class AbstractADEDBSchemaManager implements ADEDBSchemaManager {
 	}
 	
 	public void cleanupADEData(String adeId) throws SQLException {
-		SchemaMapping schemaMapping = ObjectRegistry.getInstance().getSchemaMapping();
-		AbstractDatabaseAdapter databaseAdapter = DatabaseConnectionPool.getInstance().getActiveDatabaseAdapter();
-		ConfigQueryBuilder queryBuilder = new ConfigQueryBuilder(schemaMapping, databaseAdapter);
-		Query query = null;
-		try {			
-			query = queryBuilder.buildQuery(new SimpleQuery(), new ConfigNamespaceFilter());
-		} catch (QueryBuildException e) {
-			throw new SQLException("Failed to build the query expression for cleaning up ADE data.", e);
-		}		
-
+		ADEMetadataManager adeMetadataManager = new ADEMetadataManager(connection, config);
+		SchemaMapping schemaMapping = adeMetadataManager.getMergedSchemaMapping();
 		Statement stmt = null;
-		ResultSet rs = null;			
-		try {					
+		ResultSet rs = null;
+		List<DBSplittingResult> deleteWorks = new ArrayList<>();
+		try {				
 			stmt = connection.createStatement();
-			rs = stmt.executeQuery("select o.id from " + schema + ".objectclass o, " + schema + ".ade a" 
-					+ " where o.ade_id = a.id and a.adeid = '" + adeId + "'");			
+			rs = stmt.executeQuery(
+						"select co.id, co.objectclass_id "
+						+ "FROM " + schema + ".cityobject co, " + schema + ".objectclass oc, " + schema + ".ade a "
+						+ "WHERE co.objectclass_id = oc.id "
+						+ "AND a.id = oc.ade_id "
+						+ "AND a.adeid = '" + adeId + "'");			
 			while (rs.next()) {
-				int objectclassId = rs.getInt(1);	
+				int objId = rs.getInt(1);
+				int objectclassId = rs.getInt(2);
 				AbstractObjectType<?> objectType = schemaMapping.getAbstractObjectType(objectclassId);
-				if (objectType != null) {
-					ProjectionFilter projectionFilter = new ProjectionFilter(objectType);
-					query.addProjectionFilter(projectionFilter);
-				}			
-			}						
+				deleteWorks.add(new DBSplittingResult(objId, objectType));		
+			}
 		} finally {
-			if (rs != null) 
-				rs.close();
-	
 			if (stmt != null) 
-				stmt.close();
+				stmt.close();			
 		}	
 		
-		final ReentrantLock lock = new ReentrantLock();;
-		lock.lock();		
-		try {
-			DBDeleteController deleter = new DBDeleteController(query);
-			deleter.doProcess();
-			deleter.cleanup();
-		} catch (DBDeleteException e) {
-			throw new SQLException("Error occurred: ", e);
+		int totalWorkNumber = deleteWorks.size();
+		DBDeleteWorker deleteWorker = null;
+		final ReentrantLock lock = new ReentrantLock();		
+		try {				
+			deleteWorker = new DBDeleteWorker(ObjectRegistry.getInstance().getEventDispatcher(), connection);
+			for (DBSplittingResult work: deleteWorks) {
+				deleteWorker.doWork(work);	
+				LOG.info("Remaining number of ADE objects going to be deleted: " + (--totalWorkNumber));
+			}							
 		} finally {
-			lock.unlock();
+			if (deleteWorker != null)
+				deleteWorker.shutdown();			
+			lock.lock();			
 		}
 	}
 
